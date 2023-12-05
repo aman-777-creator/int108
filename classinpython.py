f1fcfba0d69048022592f0c92ae4bd05ddff24a0
Aman Mishra <152987182+aman-777-creator@users.noreply.github.com>
2023-12-05 22:44:51 +0530

    Add files via upload

---
+++ classinpython.py
@@ -0,0 +1,10 @@
+class eform():
+    name="mukesh"
+    adress="delhi"
+    education="12th"
+    age=18
+m1=eform()
+m1.name="somesh"
+m1.adress="punjab"
+m1.age=34
+print("name is",m1.name,"adress is",m1.adress,"age is",m1.age)
---
+++ code for checking grades.py
@@ -0,0 +1,13 @@
+a = str(input("Enter the grade obtained by you: "))
+if(a=="A"):
+    print("Outstanding")
+elif(a=="B"):
+    print("Excellent")
+elif(a=="C"):
+    print("Very Good")
+elif(a=="D"):
+    print("Good")
+elif(a=="E"):
+    print("Satisfactory")
+else:
+    print("Unrecognised")
